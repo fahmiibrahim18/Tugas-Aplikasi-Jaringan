c8d05355576cbef07936891293105af2b40ea384
fahmiibrahim18 <fahmi.ibrahim20@mhs.uinjkt.ac.id>
2023-03-15 09:47:22 +0700

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,23 @@
+while True:
+    try:
+        # masukan angka
+        num = int(input("Enter Number= "))
+        if num > 1:
+            for i in range(2,num):
+                # kalo angka bisa dibagi habis (2 s.d. num)
+                if num%i == 0:
+                    print(f"{num} bukan bilangan prima.\nKarena habis dibagi {i}\n")
+                    break
+            else:
+                print(f'{num} adalah bilangan prima\n') 
+                break   
+        else:
+            print('Harus lebih dari 1')
+        
+    except:
+        print("Harus Angka!\n")
+
+    
+
+    
+
